ceb2ad2ce20914342e3f460a1efa04f9551208e1
val <valentun9@gmail.com>
2017-01-18 18:46:41 +0200

    play

--- src/app/shared/directives/map/map.directive.js
+++ src/app/shared/directives/map/map.directive.js
@@ -401,59 +401,10 @@
                 //}
 
                 var input = document.getElementById('map-search');
-                var searchBox = new google.maps.places.SearchBox(input);
-                //vm.map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+                if (input !== null) {
+                    initSearchBox(input);
+                }
 
-                vm.map.addListener('bounds_changed', function() {
-                    searchBox.setBounds(vm.map.getBounds());
-                });
-
-                var markers = [];
-                searchBox.addListener('places_changed', function() {
-                    var places = searchBox.getPlaces();
-
-                    if (places.length == 0) {
-                        return;
-                    }
-
-                    // Clear out the old markers.
-                    markers.forEach(function(marker) {
-                        marker.setMap(null);
-                    });
-                    markers = [];
-
-                    // For each place, get the icon, name and location.
-                    var bounds = new google.maps.LatLngBounds();
-                    places.forEach(function(place) {
-                        if (!place.geometry) {
-                        console.log("Returned place contains no geometry");
-                        return;
-                        }
-                        var icon = {
-                        url: place.icon,
-                        size: new google.maps.Size(71, 71),
-                        origin: new google.maps.Point(0, 0),
-                        anchor: new google.maps.Point(17, 34),
-                        scaledSize: new google.maps.Size(25, 25)
-                        };
-
-                        // Create a marker for each place.
-                        markers.push(new google.maps.Marker({
-                        map: vm.map,
-                        icon: icon,
-                        title: place.name,
-                        position: place.geometry.location
-                        }));
-
-                        if (place.geometry.viewport) {
-                        // Only geocodes have viewport.
-                        bounds.union(place.geometry.viewport);
-                        } else {
-                        bounds.extend(place.geometry.location);
-                        }
-                    });
-                    vm.map.fitBounds(bounds);
-                });
 
                 if ($scope.data !== undefined) {
                     vm.showMarkers();
@@ -506,6 +457,63 @@
                     )
                 });
             };
+
+            function initSearchBox(input) {
+                var searchBox = new google.maps.places.SearchBox(input);
+                //vm.map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+                vm.map.addListener('bounds_changed', function() {
+                    searchBox.setBounds(vm.map.getBounds());
+                });
+
+                var markers = [];
+                searchBox.addListener('places_changed', function() {
+                    var places = searchBox.getPlaces();
+
+                    if (places.length == 0) {
+                        return;
+                    }
+
+                    // Clear out the old markers.
+                    markers.forEach(function(marker) {
+                        marker.setMap(null);
+                    });
+                    markers = [];
+
+                    // For each place, get the icon, name and location.
+                    var bounds = new google.maps.LatLngBounds();
+                    places.forEach(function(place) {
+                        if (!place.geometry) {
+                            console.log("Returned place contains no geometry");
+                            return;
+                        }
+                        var icon = {
+                            url: place.icon,
+                            size: new google.maps.Size(71, 71),
+                            origin: new google.maps.Point(0, 0),
+                            anchor: new google.maps.Point(17, 34),
+                            scaledSize: new google.maps.Size(25, 25)
+                        };
+
+                        // Create a marker for each place.
+                        markers.push(new google.maps.Marker({
+                            map: vm.map,
+                            icon: icon,
+                            title: place.name,
+                            position: place.geometry.location
+                        }));
+
+                        if (place.geometry.viewport) {
+                            // Only geocodes have viewport.
+                            bounds.union(place.geometry.viewport);
+                        } else {
+                            bounds.extend(place.geometry.location);
+                        }
+                    });
+                    
+                    vm.map.fitBounds(bounds);
+                });
+            }
 
             vm.search = function (req) {
                 var geocoder = new google.maps.Geocoder();
